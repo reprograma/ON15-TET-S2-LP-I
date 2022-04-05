903bae1e770208aa05a7d209ae1eed2bb65ae0af
carolmarquezini <c.marquezini15@gmail.com>
2022-04-05 08:47:48 -0300

    adicionando ex. minecraft fase 14

---
+++ ex1-minecraft.js
@@ -0,0 +1,21 @@
+ex1-minecraft
+
+executar 4 vezes{
+    avançar;
+    quebrar bloco;
+
+    se tiver água{
+        preencher com pedra 
+    }
+    se não{
+        siga em frente
+    }
+}
+
+vire à direita;
+executar 2 vezes {
+    avançar
+}
+quebrar bloco;
+plantar;
+avançar;
