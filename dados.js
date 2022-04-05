e5b97aabd401b1327ebf05f6af36a97d1e369884
natalia2506 <natalia_cmoura@hotmail.com>
2022-04-05 12:55:46 -0300

    atualizando a pasta de exercício1 da 2 semana

--- dados.js
+++ dados.js
@@ -1,19 +1,19 @@
 // string
 
-console.log('Lilit Bandeira tem' + 33 + 'anos')
-console.log("Lilit Bandeira tem " + 19 + " anos")
-console.log(`Lilit Bandeira tem ${33} anos`)
+// console.log('Lilit Bandeira tem' + 33 + 'anos')
+// console.log("Lilit Bandeira tem " + 19 + " anos")
+// console.log(`Lilit Bandeira tem ${33} anos`)
 
-// number
+// // number
 
-console.log(3)
-console.log(9.123)
-console.log("4") // neste caso é uma string e não um number
+// console.log(3)
+// console.log(9.123)
+// console.log("4") // neste caso é uma string e não um number
 
-// objeto
+// // objeto
 
-console.table({
-    nome: "Lilit",
-    signo: "Aquário",
-    idade: 33 
-})
+// console.table({
+//     nome: "Lilit",
+//     signo: "Aquário",
+//     idade: 33 
+// })
